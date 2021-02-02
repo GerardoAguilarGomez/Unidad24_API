c1b8c2c4098a8df296bce186bdacccbb5d360b71
GerardoAguilarGomez <gerardo.aguilar@grupocastilla.es>
2021-02-02 17:55:49 +0100

    API Ejercicio 3

---
+++ API2/Models/Video.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API2.Models
+{
+    public class Video
+    {
+        private int id;
+        private string titulo;
+        private string director;
+        private int cliente_id;
+
+        public int Id { get => id; set => id = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Director { get => director; set => director = value; }
+        public int Cliente_id { get => cliente_id; set => cliente_id = value; }
+    }
+}
