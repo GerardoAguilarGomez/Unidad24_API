9385bc6a30e50bc12d81be499e657fdafe3bb1a6
GerardoAguilarGomez <gerardo.aguilar@grupocastilla.es>
2021-02-02 12:36:52 +0100

    API ejer 1

---
+++ API1/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API1.Models
+{
+    public class Cliente
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private string direccion;
+        private int dni;
+        private DateTime fecha;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public int Dni { get => dni; set => dni = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Unidad24_API
+
+- Swagger
+https://prnt.sc/y0fvli
+
+- Post
+https://prnt.sc/y0g5ha
+
+- Get
+https://prnt.sc/y0gahf
+
+- Put
+https://prnt.sc/y0ggc7
+
+- Delete
+https://prnt.sc/y0gl1n
